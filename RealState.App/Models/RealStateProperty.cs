f08262dc49203b71125c1b45c25850d67bf12bcd
Marc Fabregat <marc.fabregatb@gmail.com>
2022-08-27 17:10:47 -0600

    rename agent

--- RealState.App/Models/RealStateProperty.cs
+++ RealState.App/Models/RealStateProperty.cs
@@ -7,7 +7,7 @@
         public string Description { get; set; }
         public string PrincipalImage => Images.FirstOrDefault();
         public List<string> Images { get; set; }
-        public RealtorAgent RealtorAgent { get; set; }
+        public Agent Agent { get; set; }
         public Category Category { get; set; }
         public string Address { get; set; }
         public int Rooms { get; set; }
@@ -20,7 +20,7 @@
         public int BikeScore { get; set; }
     }
 
-    public class RealtorAgent
+    public class Agent
     {
         public string Name { get; set; }
         public string Image { get; set; }
---
+++ RealState.App/Views/AgentProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RealState.App.Views;
+
+public partial class AgentProfilePage : ContentPage
+{
+	public AgentProfilePage()
+	{
+		InitializeComponent();
+	}
+}
